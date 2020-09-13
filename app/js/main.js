69c6b086d77bc7a89b5f1c2fc274471b728d0b6c
Serial57 <thesartli@gmail.com>
2020-09-13 23:05:08 +0300

    add header slider mixer

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,21 @@
-$(function(){
+$(function () {
+
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    readOnly: true,
+    ratedFill: "#ffc000",
+    starWidth: "15px"
+  });
+
+  var mixer = mixitup('.products__inner-box');
+
+  $('.product-slider__boxslide').slick({
+    arrows: true,
+    variableWidth: true,
+    prevArrow: $('.lnr-chevron-left'),
+    nextArrow: $('.lnr-chevron-right')
+  });
 
 
 
-
-
-
-
-  
 });
